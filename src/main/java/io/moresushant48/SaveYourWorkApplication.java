a9b02dbfa0a44a606bde3f5309e4c2a2e61a19e5
moresushant48 <moresushant48@gmail.com>
2019-06-17 21:44:18 +0800

    Initial Commit, Project initialised.
    Signed-off-by: moresushant48 <moresushant48@gmail.com>

---
+++ src/main/java/io/moresushant48/SaveYourWorkApplication.java
@@ -0,0 +1,13 @@
+package io.moresushant48;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveYourWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveYourWorkApplication.class, args);
+	}
+
+}
