79ac333e1584990d6d7fd2db8c040aa29799dd56
moresushant48 <moresushant48@gmail.com>
2019-06-19 20:05:54 +0800

    Updated following : * Updated form layout (title added, succeful msg) * implemented all the required validations for the form submission. * SignUp class, holding the data from the view.
    Signed-off-by: moresushant48 <moresushant48@gmail.com>

--- src/main/java/io/moresushant48/controller/SignupController.java
+++ src/main/java/io/moresushant48/controller/SignupController.java
@@ -1,13 +1,28 @@
 package io.moresushant48.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import io.moresushant48.impl.SignupImpl;
 
 @Controller
 public class SignupController {
 	
 	@RequestMapping("/signup")
-	public String signup() {
+	public String signup(Model model) {
+		model.addAttribute("registerDetails", new SignupImpl() );
 		return "signup.html";
+	}
+	
+	@PostMapping("/signup")
+	public ModelAndView sign_up(@ModelAttribute("registerDetails") SignupImpl signupImpl, BindingResult bindingResult) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("result","Registered Successfully.");
+		return mv;
 	}
 }
---
+++ src/main/java/io/moresushant48/impl/SignupImpl.java
@@ -0,0 +1,32 @@
+package io.moresushant48.impl;
+
+public class SignupImpl {
+
+	private String mail = null;
+	private String username;
+	private String password;
+	
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
