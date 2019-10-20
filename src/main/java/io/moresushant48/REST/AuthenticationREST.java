9e6d3324e4bbd16670fe3a8494fa1661a543650f
Sushant More <moresushant48@gmail.com>
2019-10-20 17:03:33 +0530

    REST : Modularize REST Req Controllers
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- src/main/java/io/moresushant48/REST/AuthenticationREST.java
+++ src/main/java/io/moresushant48/REST/AuthenticationREST.java
@@ -1,18 +1,11 @@
-package io.moresushant48.controller;
-
-import java.util.Optional;
-
+package io.moresushant48.REST;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import io.moresushant48.Repository.UserRepository;
 import io.moresushant48.model.Role;
@@ -20,33 +13,13 @@ import io.moresushant48.model.User;
 
 @RestController
 @RequestMapping("/rest")
-public class REST {
-	
+public class AuthenticationREST {
+
 	@Autowired
 	UserRepository userRepository;
 	
 	@Autowired
 	BCryptPasswordEncoder bCryptPasswordEncoder;
-	
-	@GetMapping("/{id}")
-	@JsonIgnore
-	public Optional<User> viewList(@PathVariable("id") int id) {
-		
-		return userRepository.findById(id);
-	}
-	
-	@RequestMapping("/success")
-	public String success() {
-		
-		return "Login Successful.";
-	}
-	
-	@RequestMapping("/failed")
-	public String failed() {
-		
-		return "Login Failed.";
-	}
-	
 	
 	@PostMapping("/login")
 	public boolean login(@RequestParam("username") String username, @RequestParam("password") String password) {
@@ -90,4 +63,5 @@ public class REST {
 			}			
 		}
 	}
+	
 }
---
+++ src/main/java/io/moresushant48/REST/REST.java
@@ -0,0 +1,34 @@
+package io.moresushant48.REST;
+
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.moresushant48.Repository.UserRepository;
+import io.moresushant48.model.User;
+
+@RestController
+@RequestMapping("/rest")
+public class REST {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@GetMapping("/{id}")
+	@JsonIgnore
+	public Optional<User> viewList(@PathVariable("id") int id) {
+		
+		return userRepository.findById(id);
+	}
+}
