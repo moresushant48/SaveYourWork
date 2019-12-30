dca000d9fe638ed74dce5c3d0f379c82086dd6f9
Sushant More <moresushant48@gmail.com>
2019-12-30 17:35:40 +0530

    Sharing : Setup Public/Private file sharing completed.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

---
+++ src/main/java/io/moresushant48/Repository/AccessRepository.java
@@ -0,0 +1,9 @@
+package io.moresushant48.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.moresushant48.model.Access;
+
+public interface AccessRepository extends JpaRepository<Access, Long> {
+
+}
