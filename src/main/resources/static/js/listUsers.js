9c724e8b27a81155d71dcb8eaf042bc7ca2d6db6
Sushant More <moresushant48@gmail.com>
2020-05-03 13:36:06 +0530

    Admin/List User : Bootbox prompt for user delete confirmation.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- src/main/resources/static/js/listUsers.js
+++ src/main/resources/static/js/listUsers.js
@@ -1,17 +1,7 @@
 /**
  * Request handler for Listing users for admin.
  */
-var tblUsers;
-
-// delete file.
-function requestUsers(e,event){
-	
-	event.preventDefault();
-	$.get($(e).attr('href'),
-    	function (data, textStatus, jqXHR) {
-		tblUsers.ajax.reload();
-    });
-}
+var tblUsers; 
 
 function feedUsersTable() {
 	tblUsers = $('#usersTable').DataTable({
@@ -28,11 +18,32 @@ function feedUsersTable() {
 			{ "mData": "email" },
 			{ "mRender" : function (data, type, row) {
 				
-		       return '<a href="/admin/manage-role?id=' + row.id + '" class="fa fa-edit fa-lg text-primary mr-4"></a>' +
-		       		'<a href="/admin/delete-user?id=' + row.id + '" onclick="requestUsers(this,event)" class="fa fa-trash fa-lg text-danger"></a>';
+		       return '<input id="uid" hidden="hidden" type="text" value="' + row.id + '" />' +
+		       		'<a href="/admin/manage-role?id=' + row.id + '" class="fa fa-edit fa-lg text-primary mr-4"></a>' +
+		       		'<a href="#" class="admin-del-user fa fa-trash fa-lg text-danger"></a>';
 		                   		
 			}}
 		]
 				
 	 });
+}
+
+$(document).on('click','.admin-del-user',function(e) {
+	e.preventDefault();
+    bootbox.confirm({
+        title: "Confirm",
+        message: "Are your sure you want to delete your account? <br> <p class='text-muted'>Note : This cannot be undone.</p>",
+        backdrop: true,
+        centerVertical: true,
+        callback: function(result) { if(result) deleteUser() }
+    });
+});
+
+function deleteUser(){
+	
+	$.get("/user/account/delete/" + $('#uid').val(), function(data, textStatus, jqXHR) {
+		if(data){
+			tblUsers.ajax.reload();
+		}
+	});
 }
